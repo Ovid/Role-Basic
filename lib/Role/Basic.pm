951fe93050a30710acdd6fe3178b0f1524776ab3
Curtis 'Ovid' Poe <ovid@cpan.org>
2011-01-23 08:16:28 +0100

    Bump up version number.

--- lib/Role/Basic.pm
+++ lib/Role/Basic.pm
@@ -10,7 +10,7 @@ use Storable ();
 use Carp ();
 use Data::Dumper ();
 
-our $VERSION = '0.0801';
+our $VERSION = '0.0802';
 
 # eventually clean these up
 my ( %IS_ROLE, %REQUIRED_BY, %HAS_ROLES, %ALLOWED_BY, %PROVIDES );
@@ -449,7 +449,7 @@ Role::Basic - Just roles. Nothing else.
 
 =head1 VERSION
 
-Version 0.0801
+Version 0.0802
 
 =head1 SYNOPSIS
 

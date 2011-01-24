587c63a71eaa292b4b5e564613b0213ff8f37437
Curtis 'Ovid' Poe <ovid@cpan.org>
2011-01-24 08:05:14 +0100

    Add MyTests to MANIFEST and bump up version number.

--- lib/Role/Basic.pm
+++ lib/Role/Basic.pm
@@ -10,7 +10,7 @@ use Storable ();
 use Carp ();
 use Data::Dumper ();
 
-our $VERSION = '0.0802';
+our $VERSION = '0.0803';
 
 # eventually clean these up
 my ( %IS_ROLE, %REQUIRED_BY, %HAS_ROLES, %ALLOWED_BY, %PROVIDES );
@@ -449,7 +449,7 @@ Role::Basic - Just roles. Nothing else.
 
 =head1 VERSION
 
-Version 0.0802
+Version 0.0803
 
 =head1 SYNOPSIS
 
